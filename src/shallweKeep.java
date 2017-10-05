bf6c5ff3a76ebe85d04c4b246819696147ade909
TravisGray1 <travisogray1@gmail.com>
2017-10-05 15:36:55 -0400

    Initial commit

---
+++ src/shallweKeep.java
@@ -0,0 +1,34 @@
+//Travis Gray
+//Write an application that asks a user if they want to continue, and only stops when they type 'n'.
+//Validate your code! (Cater for values that are not 'y' or 'n')
+
+
+
+import java.util.Scanner;
+
+public class shallweKeep {
+
+    public static void main(String args[]) {
+        Scanner keyboard = new Scanner(System.in);
+        char ch1 = 110;//ASCII code for n
+        String text="n";
+        String text2 = new String("n");
+        int letter;
+        System.out.println("Type a letter");
+        text = keyboard.nextLine();
+
+
+//Notes;equals() & equalsIgnoreCase() and Both of these methods are used for comparing two strings.
+// The only difference between them is that the equals() methods considers the case while equalsIgnoreCase() methods ignores the case during comparison.
+// For e.g. The equals() method would return false if we compare the strings “TEXT” and “text” however equalsIgnoreCase() would return true.
+        while(!text.equalsIgnoreCase("N")){
+            System.out.println("Do you want to continue");
+            keyboard.nextLine();
+
+
+        }
+        }
+}
+
+
+
